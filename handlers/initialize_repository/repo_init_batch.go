99a08bdc8733428d2b8db9a66e4debed8c5a16f5
Productivity <grantchen2021@gmail.com>
2024-05-02 15:11:35 -0400

    filled in sending of grpc client request in ReportSavedFileChunks

--- handlers/initialize_repository/repo_init_batch.go
+++ handlers/initialize_repository/repo_init_batch.go
@@ -1,9 +1,13 @@
 package initializeRepository
 
 import (
+	"context"
 	"insight-repository-service/database"
+	"insight-repository-service/protobufs"
 	"log"
 	"os"
+
+	"google.golang.org/grpc"
 )
 
 type RepoInitBatch struct {
@@ -32,8 +36,7 @@ func (batch RepoInitBatch) SaveFileChunksAsBase64() error {
 		fileChunk := FileChunk{
 			UserId:   batch.SessionId,
 			FilePath: filePath,
-			// content is stored as base64 random string,
-			// this base64 string can be decoded to give us a utf8 string
+			// content is stored as base64 string which can be decoded to give us a utf8 string
 			Content:           fileData.Content,
 			ContentChunkIndex: fileData.ChunkIndex,
 			NumTotalChunks:    fileData.NumTotalChunks,
@@ -49,55 +52,44 @@ func (batch RepoInitBatch) SaveFileChunksAsBase64() error {
 func (batch RepoInitBatch) ReportSavedFileChunks() ([]string, error) {
 	var filePathsToProcess []string
 
+	conn, err := grpc.Dial(os.Getenv("REPOSITORY_SYNC_SERVICE_ADDRESS"), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := protobufs.NewRepositorySyncServiceClient(conn)
+	stream, err := client.ReportSavedFileChunk(context.Background())
+	if err != nil {
+		log.Fatalf("error creating stream: %v", err)
+	}
+
 	for filePath, fileData := range batch.Files {
-		// FILL ME IN
-		// send the below data to the grpc server
-		// make concurrent (bidirectional streaming?)
-		log.Println(batch.SessionId, filePath, fileData.ChunkIndex, fileData.NumTotalChunks)
-		isLastReportedChunkForFile := true
-		if isLastReportedChunkForFile {
+		err := stream.Send(&protobufs.ReportSavedFileChunkRequest{
+			UserId:         batch.SessionId,
+			FilePath:       filePath,
+			ChunkIndex:     int32(fileData.ChunkIndex),
+			NumTotalChunks: int32(fileData.NumTotalChunks),
+		})
+
+		if err != nil {
+			log.Fatalf("error sending request: %v", err)
+		}
+
+		resp, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("error receiving response: %v", err)
+
+		}
+
+		if resp.IsLastChunk {
 			filePathsToProcess = append(filePathsToProcess, filePath)
 		}
 	}
+
+	if err = stream.CloseSend(); err != nil {
+		log.Fatalf("error closing stream: %v", err)
+	}
+
 	return filePathsToProcess, nil
 }
-
-// make this concurrent
-// func (batch RepoInitBatch) ReportSaveRawSuccess() ([]string, error) {
-// 	fullySavedFilePaths := []string{}
-
-// 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
-// 	if err != nil {
-// 		log.Fatalf("failed to dial: %v", err)
-// 	}
-// 	defer conn.Close()
-// 	client := pb.NewRepositoryLockClient(conn)
-
-// 	stream, err := client.HandleSaveRawSuccess(context.Background())
-// 	if err != nil {
-// 		return fullySavedFilePaths, err
-// 	}
-
-// 	for filePath, fileData := range batch.Files {
-// 		err := stream.Send(&pb.ReportSaveRawSuccessRequest{
-// 			UserId:            batch.SessionId,
-// 			FilePath:          filePath,
-// 			ContentChunkIndex: int32(fileData.ChunkIndex),
-// 			NumTotalChunks:    int32(fileData.NumTotalChunks),
-// 		})
-
-// 		if err != nil {
-// 			return fullySavedFilePaths, err
-// 		}
-
-// 		res, err := stream.Recv()
-// 		if err != nil {
-// 			return fullySavedFilePaths, err
-// 		}
-
-// 		if res.AllChunksSaved {
-// 			fullySavedFilePaths = append(fullySavedFilePaths, res.FilePath)
-// 		}
-// 	}
-// 	return fullySavedFilePaths, nil
-// }
