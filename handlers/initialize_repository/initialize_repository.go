51bdf2c616a8e1b7dabf4cacb529eeffe14a4a26
Productivity <grantchen2021@gmail.com>
2024-06-02 18:50:03 -0400

    created connection to file-structure-service

--- handlers/initialize_repository/initialize_repository.go
+++ handlers/initialize_repository/initialize_repository.go
@@ -1,10 +1,14 @@
 package initializeRepository
 
 import (
+	"context"
+	"insight-repository-service/protobufs"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
 )
 
 func unpackRequest(c *gin.Context) (RepoInitBatch, error) {
@@ -20,6 +24,44 @@ func unpackRequest(c *gin.Context) (RepoInitBatch, error) {
 
 	batch.SessionId = sessionId
 	return batch, err
+}
+
+func getFileStructure(userId string, filePaths []string) error {
+	conn, err := grpc.Dial(os.Getenv("FILE_STRUCTURE_SERVICE_ADDRESS"), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := protobufs.NewFileStructureServiceClient(conn)
+	stream, err := client.ExtractStructure(context.Background())
+	if err != nil {
+		log.Fatalf("error creating stream to file structure service: %v", err)
+	}
+
+	for _, filePath := range filePaths {
+		err := stream.Send(&protobufs.File{
+			UserId:   userId,
+			FilePath: filePath,
+		})
+
+		if err != nil {
+			log.Fatalf("error sending request: %v", err)
+		}
+
+		_, err = stream.Recv()
+		if err != nil {
+			log.Fatalf("error receiving response: %v", err)
+
+		}
+	}
+
+	if err = stream.CloseSend(); err != nil {
+		log.Fatalf("error closing stream: %v", err)
+	}
+
+	return err
+
 }
 
 func InitializeRepository(c *gin.Context) {
@@ -42,10 +84,7 @@ func InitializeRepository(c *gin.Context) {
 		return
 	}
 
-	// checkpoint
-	for _, filePath := range filePathsToProcess {
-		log.Println(filePath)
-	}
+	getFileStructure(batch.SessionId, filePathsToProcess)
 
 	// syntax parse each file in filePathsToProcess
 	// semantically summarize each file in filePathsToProcess
